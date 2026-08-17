5f9e4bf7ceb8ba8d41fce86d85919298e20e4d25
Philipp Kleber (axiphi) <philipp.t.kleber@gmail.com>
2026-08-17 19:23:57 +0200

    unify default mongo address in start-up scripts

--- root_orchestrator/system-manager-python/start-up.sh
+++ root_orchestrator/system-manager-python/start-up.sh
@@ -3,7 +3,7 @@
 export FLASK_ENV=development
 # export FLASK_DEBUG=False # TRUE for logging
 
-export ROOT_MONGO_URL=172.17.0.0
+export ROOT_MONGO_URL=localhost
 export ROOT_MONGO_PORT=10007
 
 export ROOT_SCHEDULER_URL=localhost
